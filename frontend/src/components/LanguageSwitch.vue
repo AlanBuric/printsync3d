c69223c1e5db5338aa72e6838ccaf6d3d430a1e2
Alan Burić <134159250+AlanBuric@users.noreply.github.com>
2024-12-05 00:53:33 +0100

    feat: improve LanguageSwitch.vue

--- frontend/src/components/LanguageSwitch.vue
+++ frontend/src/components/LanguageSwitch.vue
@@ -1,9 +1,15 @@
+<script setup lang="ts">
+  const languages: Record<string, string> = {
+    'en-us': 'English',
+    'hr-hr': 'Hrvatski',
+    'it': 'Italiano'
+  };
+</script>
+
 <template>
   <select
     class="bg-transparent text-lg font-light text-zinc-800 dark:text-zinc-200 rounded-xl cursor-pointer"
     title="Select the display language">
-    <option value="en-us">English</option>
-    <option value="hr-hr">Hrvatski</option>
-    <option value="it-it">Italiano</option>
+    <option v-for="[value, text] in Object.entries(languages)" :value="value" :key="value" class="dark:bg-zinc-800 border-0">{{ text }}</option>
   </select>
 </template>
