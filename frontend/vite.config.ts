596e1c99f65c89165ecb8a3bbf91d58c4310d9b4
Alan Burić <134159250+AlanBuric@users.noreply.github.com>
2024-12-19 03:46:21 +0100

    feat: proxy frontend to backend in dev mode

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,26 @@
-import { fileURLToPath, URL } from 'node:url';
-
+import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
   plugins: [
     vue()
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  preview: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
     }
   },
   build: {
     emptyOutDir: true
-  }
+  },
+  test: {
+    environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'e2e/**'],
+    root: fileURLToPath(new URL('./', import.meta.url)),
+  },
 });
