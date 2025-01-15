c48a083fd98062663ac90917cd2463fee6d205ef
Alan Burić <134159250+AlanBuric@users.noreply.github.com>
2025-01-15 01:36:48 +0100

    feat(api): add server close message

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,8 +4,7 @@ import chalk from 'chalk';
 import { connectDatabase, getDatabase } from './database/database.js';
 import PrintSync3DConfig from './config/config.js';
 import ModelService from './routes/model/service.js';
-
-const handleShutdown = async () => getDatabase().write();
+import getLoggingPrefix from './util/logging.js';
 
 new PrintSync3DConfig();
 new PrinterService();
@@ -13,19 +12,26 @@ new ModelService();
 
 await connectDatabase();
 
-createApplication()
-  .listen(PrintSync3DConfig.PORT, () => {
-    console.log(
-      chalk.blueBright.bold('✔ PrintSync3D service is up and running:'),
-      '\n',
-      chalk.blue(
-        `➥ API routes: ${chalk.underline(`http://localhost:${PrintSync3DConfig.PORT}/api`)}`,
-      ),
-    );
+const server = createApplication().listen(PrintSync3DConfig.PORT, () => {
+  console.log(
+    chalk.blueBright.bold('✔ PrintSync3D service is up and running:'),
+    '\n',
+    chalk.blue(
+      `➥ API routes: ${chalk.underline(`http://localhost:${PrintSync3DConfig.PORT}/api`)}`,
+    ),
+  );
 
-    PrinterService.refreshConnections();
-  })
-  .on('close', handleShutdown);
+  PrinterService.refreshConnections();
+});
 
+const handleShutdown = async () => {
+  console.log(chalk.blueBright.bold(`${getLoggingPrefix()} PrintSync3D is shutting down.`));
+  await getDatabase().write();
+  server.close((error) => {
+    console.error('An error occurred while shutting downthe PrintSync3D server', error);
+  });
+};
+
+server.on('close', handleShutdown);
 process.on('SIGTERM', handleShutdown);
 process.on('SIGINT', handleShutdown);
